fdd47e03c3a0ab606c59b5423d96d1621b3ce816
Marcos André <marcosandreazevedo78@gmail.com>
2022-11-14 17:51:34 -0300

    Pequenos ajustes.

--- montarpdf.php
+++ montarpdf.php
@@ -23,15 +23,45 @@ if($anoBissexto) {
 	$qtdDiasMes[2] += 1;
 }
 
+$nomeMes = "";
+
+switch ($mes) {
+	case 1: $nomeMes = "Janeiro";
+		break;
+	case 2: $nomeMes = "Fevereiro";
+		break;
+	case 3: $nomeMes = "Março";
+		break;
+	case 4: $nomeMes = "Abril";
+		break;
+	case 5: $nomeMes = "Maio";
+		break;
+	case 6: $nomeMes = "Junho";
+		break;
+	case 7: $nomeMes = "Julho";
+		break;
+	case 8: $nomeMes = "Agosto";
+		break;
+	case 9: $nomeMes = "Setembro";
+		break;
+	case 10: $nomeMes = "Outubro";
+		break;
+	case 11: $nomeMes = "Novembro";
+		break;
+	case 12: $nomeMes = "Dezembro";
+		break;
+}
+
 $html = "";
 
 $html .= "<!DOCTYPE html>
 <html lang='pt-br'>
 	<head>
-		<title>Folha Gerada</title>
+		<title>Folha de $nomeMes Gerada</title>
 		<link rel='stylesheet' type='text/css' href='estilo.css'>
 	</head>
 	<body>
+		<h3>FOLHA DE PONTO DE ".strtoupper($nomeMes)."</h3>
 		<table>
 			<thead>
 				<tr>
@@ -43,7 +73,7 @@ $html .= "<!DOCTYPE html>
 					<td class='colunaData'>DATA</td>
 					<td class='colunaEntrada'>ENTRADA</td>
 					<td class='colunaAssinatura'>ASSINATURA</td>
-					<td class='colunaSaida'>SAIDA</td>
+					<td class='colunaSaida'>SAÍDA</td>
 				</tr>";
 
 $sabado = false;
@@ -87,12 +117,14 @@ $html .= "\n\t\t\t</tbody>
 	</body>
 </html>";
 
+$html .= $html;
+//die($html);
 $dompdf->loadHtml($html);
 
 $dompdf->setPaper('A4');
 
 $dompdf->render();
 
-$dompdf->stream("Folha.pdf", ["Attachment" => false]);
+$dompdf->stream("FolhaDe$nomeMes.pdf", ["Attachment" => false]);
 
 ?>
