47b1b4cc8bfc154074018819604f82488be47f62
Marcos André <marcosandreazevedo78@gmail.com>
2022-11-19 01:03:19 -0300

    Refatoração.

--- editar_usuario.php
+++ editar_usuario.php
@@ -1,13 +1,13 @@
 <?php
-    if(isset($_GET["editarUsuario"])){
-		$id = $_GET["editarUsuario"];
+    if(isset($_GET["editarUsuario"])) {
+        $id = $_GET["editarUsuario"];
 
         try {
             include('conexaoBD.php');
             
-            $usuario = $pdo->query("SELECT * FROM usuarios WHERE id = $id");
+            $consulta = $pdo->query("SELECT * FROM usuarios WHERE id = $id");
 
-            if($usuario = $consulta->fetch(PDO::FETCH_ASSOC)){
+            if($usuario = $consulta->fetch(PDO::FETCH_ASSOC)) {
                 $id = $usuario['id'];
                 $nome = $usuario['nome'];
                 $funcao = $usuario['funcao'];
@@ -16,44 +16,55 @@
                 $telefone = $usuario['telefone'];
             }
         } catch(PDOException $e) {
-            echo 'Error: ' . $e->getMessage();
+            echo 'Error: '.$e->getMessage();
         }
     }
 ?>
 
-<br/>
-<form method="POST" action="">
-	<label>Nome:</label>
-	<input type="text" name="nome" value="<?php echo $nome; ?>" required/>
-	<br/><br/>
-	<label>Idade:</label>
-	<input type="text" name="funcao" value="<?php echo $funcao; ?>" required/>
-	<br/><br/>
-	<label>Cidade:</label>
-	<input type="text" name="condicao" value="<?php echo $condicao; ?>" required/>
-	<br/><br/>
-	<label>UF:</label>
-	<input type="text" name="email" value="<?php echo $email; ?>" required/>
-	<br/><br/>
-	<label>Email:</label>
-	<input type="text" name="telefone" value="<?php echo $telefone; ?>" required/>
-	<br/><br/>
-	<input type="submit" name="atualizarDados" value="Atualizar Usuário"/>
+<br>
+<form method="POST">
+    <hr>
+    <label>Nome:</label>
+    <input type="text" name="nome" value="<?php echo $nome; ?>" required>
+    <br><br>
+    <label>Função:</label>
+    <select name="funcao" required>
+        <option value="<?php echo $funcao; ?>" selected><?php echo $funcao; ?></option>
+        <option value="Servidor">Servidor</option>
+        <option value="Terceirizado">Terceirizado</option>
+        <option value="Estagiario">Estagiario</option>
+    </select>
+    <br><br>
+    <label>Condição:</label>
+    <select name="condicao" required>
+        <option value="<?php echo $condicao; ?>" selected><?php echo $condicao; ?></option>
+        <option value="Trabalhando">Trabalhando</option>
+        <option value="Ferias">Férias</option>
+        <option value="Afastado">Afastado</option>
+    </select>
+    <br><br>
+    <label>Email:</label>
+    <input type="text" name="email" value="<?php echo $email; ?>">
+    <br><br>
+    <label>Telefone:</label>
+    <input type="text" name="telefone" value="<?php echo $telefone; ?>">
+    <br><br>
+    <input type="submit" name="atualizarDados" value="Atualizar Usuário">
 </form>
 
 <?php
-	if(isset($_POST["atualizarDados"])){
-		$nome = $_POST["nome"];
-		$funcao = $_POST["funcao"];
-		$condicao = $_POST["condicao"];
-		$email = $_POST["email"];
-		$telefone = $_POST["telefone"];
+    if(isset($_POST["atualizarDados"])) {
+        $nome = $_POST["nome"];
+        $funcao = $_POST["funcao"];
+        $condicao = $_POST["condicao"];
+        $email = $_POST["email"];
+        $telefone = $_POST["telefone"];
 
         try {
             include('conexaoBD.php');
           
-            $stmt = $pdo->prepare('UPDATE usuarios 
-                SET nome = :nome, funcao = :funcao, condicao = :condicao, email = :email, telefone = :telefone
+            $stmt = $pdo->prepare('UPDATE usuarios SET 
+                nome = :nome, funcao = :funcao, condicao = :condicao, email = :email, telefone = :telefone
                 WHERE id = :id');
 
             $stmt->execute(array(
@@ -65,12 +76,12 @@
                 ':telefone' => $telefone
             ));
           
-            if($stmt->rowCount()){
+            if($stmt->rowCount()) {
                 echo "<script>alert('Dados Atualizados!')</script>";
                 echo "<script>window.open('listar_usuarios.php', '_self')</script>";
             }
         } catch(PDOException $e) {
-        echo 'Error: ' . $e->getMessage();
+            echo 'Error: '.$e->getMessage();
         }
-	}
+    }
 ?>
