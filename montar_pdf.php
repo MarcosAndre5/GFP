de3af6a3175f5f42d40f1c76793eab6a3d46691d
Administrador <hitaloemanoel@uern.br>
2023-02-17 12:15:16 -0300

    Pequenos ajustes.

--- montar_pdf.php
+++ montar_pdf.php
@@ -2,6 +2,7 @@
 <html lang='pt-br'>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Folha de <?= $nomeMes[$mes] ?> de <?= $campus ?> Gerada</title>
         <link rel='stylesheet' type='text/css' href='frontend/estilo.css'>
     </head>
